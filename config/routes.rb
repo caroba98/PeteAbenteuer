bf7d9e20ea8a9f165d8e4400d213bfebea33751d
Caroline Baumeister <caroline@baumeister-dv.de>
2024-11-22 18:55:03 +0100

    Anfügen der homepage von alter version

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root to: "articles#index"
+  root to: "pages#home"
   devise_for :users
   resources :articles
+  get 'partner', to: 'pages#partner', as: :partner
 
   # Defines the root path route ("/")
   # root "posts#index"
