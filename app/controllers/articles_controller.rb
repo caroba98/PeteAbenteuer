907392cd2d0f38341110f9f5fd6980b4d2b41906
Caroline Baumeister <caroline@baumeister-dv.de>
2024-11-22 17:33:31 +0100

    Fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  # before_action :authenticate_user!, except: [:index, :show]
 
   # GET /articles or /articles.json
   def index
@@ -8,6 +9,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1 or /articles/1.json
   def show
+    @article = Article.find(params[:id])
   end
 
   # GET /articles/new
@@ -22,39 +24,28 @@ class ArticlesController < ApplicationController
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.save
+      flash[:success] = 'Dein Artikel wurde erfolgreich erstellt.'
+      redirect_to articles_path
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.update(queen_params)
+      flash[:success] = 'Artikel erfolgreich bearbeitet.'
+      redirect_to articles_path
+    else
+      render :edit
     end
   end
 
   # DELETE /articles/1 or /articles/1.json
   def destroy
-    @article.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @article.destroy
+    redirect_to dashboard_path, notice: 'Artikel erfolgreich gelöscht.'
   end
 
   private
